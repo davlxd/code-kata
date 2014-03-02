53bbf8396d6bf21c3e1bb862483141da11905b08
lxd <lxd@Air>
2014-03-02 13:56:56 +0800

    Make 5th test pass

--- kata-02/karate_chop.rb
+++ kata-02/karate_chop.rb
@@ -1,14 +1,20 @@
 class KarateChop
-  def chop(array, target)
+  def do_chop(array, target, start_index, end_index)
     return -1 if array.length == 0
     return 0 if array.length == 1 and array[0] == target
     return -1 if array.length == 1 and array[0] != target
 
-    mid = array.length / 2
+    mid = (start_index + end_index) / 2
     if array[mid] == target
       return mid
     elsif array[mid] > target
-      chop(array[0...mid], target)
+      do_chop(array, target, 0, mid)
+    else
+      do_chop(array, target, mid + 1, end_index)
     end
+  end
+
+  def chop(array, target)
+    do_chop(array, target, 0, array.length)
   end
 end
