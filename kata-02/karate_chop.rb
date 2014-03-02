df2eac530a0df2f2cfbc110e6e01cd794a910b45
lxd <lxd@Air>
2014-03-02 13:35:20 +0800

    Add 2nd test; passed

--- kata-02/karate_chop.rb
+++ kata-02/karate_chop.rb
@@ -1,5 +1,7 @@
 class KarateChop
   def chop(array, target)
-    return -1 if array.length = 0
+    return -1 if array.length == 0
+    return 0 if array.length == 1 and array[0] == target
+    return -1 if array.length == 1 and array[0] != target
   end
 end
--- kata-02/karate_chop_spec.rb
+++ kata-02/karate_chop_spec.rb
@@ -2,8 +2,19 @@ require './karate_chop'
 
 
 describe KarateChop do
-  it "should return not found when search from empty array" do
+  it 'should return not found when search from empty array' do
     karate_chop = KarateChop.new
     karate_chop.chop([], 5).should eq(-1)
+  end
+end
+
+describe KarateChop do
+  it 'should find target from one-element array' do
+    karate_chop = KarateChop.new
+    karate_chop.chop([1], 1).should eq(0)
+  end
+  it 'should return not found if target does not in one-element array' do
+    karate_chop = KarateChop.new
+    karate_chop.chop([1], 2).should eq(-1)
   end
 end
